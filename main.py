563e44f9b1ec53b4b7efaa3b8c645fbb73d81885
1mr-newton <eaidoo015@gmail.com>
2024-04-02 20:16:20 +0000

    Add .gitignore and main.py files

---
+++ main.py
@@ -0,0 +1,24 @@
+from telethon import TelegramClient, events
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_id: str | None = os.getenv("API_ID")
+api_hash: str | None = os.getenv("API_HASH")
+bot_token: str | None = os.getenv("BOT_TOKEN")
+
+assert api_id is not None, "API_ID is not set"
+assert api_hash is not None, "API_HASH is not set"
+assert bot_token is not None, "BOT_TOKEN is not set"
+
+client = TelegramClient("anon", int(api_id), api_hash).start(bot_token=bot_token)
+
+
+@client.on(events.NewMessage)  # type: ignore
+async def handler(event):
+    print(event.raw_text)
+    await event.reply("Hello!")
+
+
+client.run_until_disconnected()
